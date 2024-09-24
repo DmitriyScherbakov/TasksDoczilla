1d2d68d3121c9ca1e36fde7f038b4e95ed44e9c0
DmitriyScherbakov <scherbakovdmitriy69@gmail.com>
2024-09-25 00:00:49 +0300

    create project

---
+++ Task2Doczilla/TaskAboutStudents/src/main/java/com/example/taskaboutstudents/TaskAboutStudentsApplication.java
@@ -0,0 +1,12 @@
+package com.example.taskaboutstudents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskAboutStudentsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TaskAboutStudentsApplication.class, args);
+    }
+}
+
