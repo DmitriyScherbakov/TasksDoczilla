4212ceb5e799d4df92287338efe188070d426c3f
DmitriyScherbakov <scherbakovdmitriy69@gmail.com>
2024-10-03 01:40:41 +0300

    css

--- Task3Doczilla/Task3Doczilla/src/main/java/org/example/task3doczilla/AppConfig.java
+++ Task3Doczilla/Task3Doczilla/src/main/java/org/example/task3doczilla/AppConfig.java
@@ -3,12 +3,21 @@ package org.example.task3doczilla;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class AppConfig {
+public class AppConfig implements WebMvcConfigurer {
 
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
     }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/static/**")
+                .addResourceLocations("classpath:/static/");
+    }
+
 }
